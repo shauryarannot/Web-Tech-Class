11dfec2f9a75befdca96d8adad5c2380ca51c09f
shauryarannot <117192500+shauryarannot@users.noreply.github.com>
2024-02-29 20:01:01 +0530

    script.js

--- inputBox/script.js
+++ inputBox/script.js
@@ -1,18 +1,20 @@
-const input=parseInt(document.querySelector('#ageInput').value);
-const message=document.querySelector('p');
+let ageInput = document.getElementById("ageInput").value;
+let ageGroupElement = document.getElementById("age-group");
+let button = document.querySelector('button');
 
-function ageGroup(input)
-{
-    if(input>=0 && input<=12){
-        message.innerHTML="Child";
-    }
-    else if(input>=13 && input<=18){
-        message.innerHTML='Teenager';
-    }
-    else if(input>=18 && input<=55){
-        message.innerHTML='Adult';
-    }
-    else{
-        message.innerHTML='Senior Citizen';
+button.addEventListener('click',function(){
+    calculateAgeGroup();
+});
+
+function calculateAgeGroup(ageInput) {
+
+    if (ageInput <= 12) {
+      ageGroupElement.textContent = "Child";
+    } else if (ageInput <= 19) {
+      ageGroupElement.textContent = "Teenage";
+    } else if (ageInput <= 35) {
+      ageGroupElement.textContent = "Young";
+    } else {
+      ageGroupElement.textContent = "Senior citizen";
     }
 }
